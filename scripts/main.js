0160ea2c7b245c83fdcc2eacfd2e1d15487fbb71
Sergey <kochkares@gmail.com>
2019-06-16 22:23:41 +0300

    v2

--- scripts/main.js
+++ scripts/main.js
@@ -7,4 +7,12 @@ function getData(callback){
   callback();
 }
 
+
+setTimeout(function(){
+  function randomData(){
+    let randValue = Math.random();
+    alert(randValue);
+  }
+}, 2000);
+
 getData(randomData);
